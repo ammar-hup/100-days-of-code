a67851c5730802c51d81d2d5c64a88a45fc34c35
MaGnsi0 <magnsi0o0@gmail.com>
2020-08-08 07:54:32 +0200

    day 2:implemented every member function of the project classes, finalized main function and program structure and practiced a bit on file handling using C++

--- banking system project/banking sys.cpp
+++ banking system project/banking sys.cpp
@@ -11,6 +11,7 @@
 class opt_error {};
 class balance_errror {};
 class security_error {};
+class data_missing {};
 //program main classes
 class account
 {
@@ -25,7 +26,7 @@ class account
 	public:
 		//constructors
 		account ();
-		account	(ll acc_num, std::string acc_name, ll acc_balance, std::string account_pass);
+		account	(std::string acc_name, ll acc_balance, std::string account_pass);
 		//accessors
 		ll get_acc_num ();
 		std::string	get_acc_name ();
@@ -48,20 +49,26 @@ class bank
 	//properties
 	private:
 		std::map<ll, account> accs_list;	//map structure to list the bank accounts
-		std::string bank_password = "mgbankcpp";
+		std::string bank_password;
 	//class behaviors
 	public:
 		//constructors
 		bank ();
+		//accessors
+		std::string get_bank_password ();
+		//mutators
+		void set_bank_password (std::string bank_password);
 		//facilitators
 		account open_acc (std::string acc_name, ll acc_balance, std::string account_pass);
-		account view_acc (std::string account_pass);
-		account deposit (std::string acc_name);
-		account withdraw (std::string account_pass);
-		void close_acc (std::string account_pass);
-		int bank_id (std::string bank_password);
+		account view_acc (ll acc_num);
+		account deposit (ll acc_num, long double amount);
+		account withdraw (ll acc_num, long double amount);
+		void close_acc (ll acc_num);
 		void show_accs_list ();
-		ll total_balance ();
+		ll total_budget ();
+		//enquiries
+		bool is_bank_pass_true (std::string bank_password);
+		bool is_acc_exist (ll acc_num, std::string acc_name, std::string acc_passs);
 		//destructor
 		~bank ();
 };
@@ -81,7 +88,7 @@ int main ()
 	//an option button for choosing the category
 	int option_0;
 	//bank object for services calling
-	bank b;
+	bank mgb;
 	std::string bank_password;
 	//account object and its properties to store users inputs
 	account acc;
@@ -94,20 +101,19 @@ int main ()
 	do
 	{
 		//intial menu
-		std::cout << "										Welcome to MG Banking System\n\n";
-		std::cout << "						For Clients Services Enter (1).\n";
-		std::cout << "						For Managers Services Ebter (2).\n";
-		std::cout << "						For Exit The System Enter (3).\n";
+		std::cout << "Welcome to MG Banking System\n\n";
+		std::cout << "For Clients Services Enter (1).\n";
+		std::cout << "For Managers Services Ebter (2).\n";
+		std::cout << "For Exit The System Enter (3).\n";
 		//option picker
 		std::cout << "Enter Your Choice : ";
 		std::cin >> option_0;
 		//exception handling for choosing nonexisting number
 		try
 		{
-			if (option_0 != 1 || option_0 != 2 || option_0 != 3)
+			if (option_0 != 1 || option_0 != 2 || option_0 != 3 || option_0 != 4)
 				throw opt_error();
 		}
-
 		catch (opt_error r)
 		{
 
@@ -118,7 +124,7 @@ int main ()
 		{
 			//clearing screen and starting the client program
 			system ("CLS");
-			std::cout << "										Welcome to Clients Service\n\n";
+			std::cout << "Welcome to Clients Service\n\n";
 			//option button for clients
 			int option_1;
 			//clients menu
@@ -134,7 +140,7 @@ int main ()
 			//exception handling for choosing nonexisting number
 			try
 			{
-				if (option_1 != 1 || option_1 != 2 || option_1 != 3 || option_1 != 4 || option_1 != 5 || option_1 != 6)
+				if (option_1 != 1 || option_1 != 2 || option_1 != 3 || option_1 != 4 || option_1 != 5 || option_1 != 6 || option_0 != 4)
 					throw opt_error ();
 			}
 			catch (opt_error r)
@@ -163,7 +169,7 @@ int main ()
 		{
 			//clearing screen and starting the managers program
 			system ("CLS");
-			std::cout << "										Welcome to Managers Service\n\n";
+			std::cout << "Welcome to Managers Service\n\n";
 			//option button for managers
 			int option_2;
 			//managers menu
@@ -176,7 +182,7 @@ int main ()
 
 			try
 			{
-				if (option_2 != 1 || option_2 != 2 || option_2 != 3)
+				if (option_2 != 1 || option_2 != 2 || option_2 != 3 || option_0 != 4)
 					throw opt_error();
 			}
 
@@ -199,18 +205,20 @@ int main ()
 	while (option_0 != 3);
 	//terminate program
 	system ("CLS");
-	std::cout << "\n\n\n\n\n\n 													Press any Button to Close...\n";
+	std::cout << "\n\n\n\n\n\nPress any Button to Close...\n";
 	{int x;std::cin>>x;}
 }
 //end of the main function
 //implementation of account methods
 	account::account ()
-	{}
-	account::account (ll acc_num, std::string acc_name, ll acc_balance, std::string account_pass)
+	{
+
+	}
+	account::account (std::string acc_name, ll acc_balance, std::string account_pass)
 	{
 		++next_acc_num;
 
-		this -> acc_num = acc_num;
+		acc_num = next_acc_num;
 		this -> acc_name = acc_name;
 		this -> acc_balance = acc_balance;
 		this -> account_pass = account_pass;
@@ -240,5 +248,133 @@ int main ()
 		if (acc_balance - amount < MIN_BALANCE)
 			throw balance_errror ();
 		acc_balance -= amount;
+	}
+	std::ofstream& operator<< (std::ofstream &ofs,account &acc)
+	{
+		ofs << "Account Number : " << acc.get_acc_num () << std::endl;
+		ofs << "Account Name : " << acc.get_acc_name () << std::endl;
+		ofs << "Account Password : " << acc.get_acc_pass () << std::endl;
+		ofs << "Account Balance : " << acc.get_acc_balance () << std::endl;
+		return ofs;	
+	}
+	std::ifstream& operator>> (std::ifstream &ifs,account &acc)
+	{
+		ifs >> acc.acc_num;
+		getline (ifs, acc.acc_name) ;
+		getline (ifs, acc.account_pass) ;
+		ifs >> acc.acc_balance;
+		return ifs;
+	}
+	std::ostream& operator<< (std::ostream &os,account &acc)
+	{
+		os << "Account Number : " << acc.get_acc_num () << std::endl;
+		os << "Account Name : " << acc.get_acc_name () << std::endl;
+		os << "Account Password : " << acc.get_acc_pass () << std::endl;
+		os << "Account Balance : " << acc.get_acc_balance () << std::endl;
+		return os;
+	}
+//implementation of bank methods
+	bank::bank ()
+	{
+		account acc;
+		std::ifstream bank_data;
+		bank_data.open ("bank.data");
+
+		if(!bank_data)
+			throw data_missing ();
+		while (!bank_data.eof ())
+		{
+			bank_data >> acc;
+			accs_list.insert (std::pair<ll, account> (acc.get_acc_num(), acc));
+		}
+
+		account::set_last_acc_num (acc.get_acc_num());
+		bank_data.close();
+	}
+	std::string bank::get_bank_password ()
+	{
+		return bank_password;
+	}
+	void bank::set_bank_password (std::string bank_password)
+	{
+		this -> bank_password = bank_password;
+	}
+	account bank::open_acc (std::string acc_name, ll acc_balance, std::string account_pass)
+	{
+		std::ofstream bank_data;
+
+		account acc (acc_name, acc_balance, account_pass);
+		accs_list.insert (std::pair<ll, account>(acc.get_acc_num (), acc));
+
+		bank_data.open ("bank.data", std::ios::trunc);
+		std::map<ll, account>::iterator itr;
+		for(itr = accs_list.begin (); itr != accs_list.end (); itr++)
+			bank_data << itr -> second;
+
+		bank_data.close ();
+		return acc;
+	}
+	account bank::view_acc (ll acc_num)
+	{
+		std::map<ll, account>::iterator itr = accs_list.find(acc_num);
+		return itr -> second;	
+	}
+	account bank::deposit (ll acc_num, long double amount)
+	{
+		std::map<ll, account>::iterator itr = accs_list.find(acc_num);
+		itr -> second.deposit (amount);
+		return itr -> second;
+	}
+	account bank::withdraw (ll acc_num, long double amount)
+	{
+		std::map<ll, account>::iterator itr = accs_list.find(acc_num);
+		itr -> second.withdraw (amount);
+		return itr -> second;
+	}
+	void bank::close_acc (ll acc_num)
+	{
+		std::map<ll, account>::iterator itr = accs_list.find(acc_num);
+		accs_list.erase (acc_num);
+	}
+	void bank::show_accs_list ()
+	{
+
+	}
+	ll bank::total_budget ()
+	{
+		ll sum = 0;
+		std::map<ll, account>::iterator itr;
+		for(itr = accs_list.begin (); itr != accs_list.end (); ++itr)
+		{
+			sum += itr -> second.get_acc_balance ();
+		}
+		return sum;
+	}
+	bool bank::is_bank_pass_true (std::string bank_password)
+	{
+		if (bank_password == get_bank_password ())
+			return true;
+		else return false;
+	}
+	bool bank::is_acc_exist (ll acc_num, std::string acc_name, std::string acc_pass)
+	{
+		std::map<ll, account>::iterator itr=accs_list.find(acc_num);
+
+		if (itr -> second.get_acc_name () == acc_name && itr -> second.get_acc_pass () == acc_pass)
+		{
+			return true;
+		}
+		else return false;
+	}
+	bank::~bank ()
+	{
+		std::ofstream bank_data;
+		bank_data.open ("bank.data", std::ios::trunc);
+
+		std::map<ll, account>::iterator itr;
+		for (itr = accs_list.begin (); itr != accs_list.end (); ++itr)
+			bank_data << itr -> second;
+
+		bank_data.close();
 	}
 //end of the program
