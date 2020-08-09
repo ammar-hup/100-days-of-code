9fe1318f05265f0ffef4bde5ae999bb92cfccf55
Ammar <amar.link8@gmail0com>
2020-08-09 21:15:48 +0200

    changes

--- my projects/mycode .cpp
+++ my projects/mycode .cpp
@@ -22,7 +22,7 @@ class Account
     long get_acc_no();
     string get_acc_name();
     float get_acc_balance();
-    float debosit(float amount);
+    float deposit(float amount);
     float withdraw(float amount);
     static void setLastAccountNumber(long accountnumber);
     static long getLastAccountNumber();
@@ -42,27 +42,38 @@ class Bank
     //constructors
     Bank ();
     //Actions
-    Account open_acc(string acc_name ,float balance);
-    account show_acc (long acc_no);
-    Account diposite(long acc_no ,float amount);
+    Account open_acc(string acc_name ,float acc_balance);
+    Account show_acc (long acc_no);
+    Account deposit(long acc_no ,float amount);
     Account withdraw(long acc_no ,float amount);
     float total_money ();
     void close_acc(long acc_no);
     void show_all_accs();
-    bool is_acc_exist (long acc_no, string acc_name);
+    bool is_acc_exist (long acc_no);
     //destructor
     ~Bank();
 };
 long Account::next_acc_no = 0;
 int main()
 {
-    Bank b();
+    Bank b;
     Account acc;
     long choice,accnum;
     string name;
     float balance,amount;
 
-    //make a menu
+//make a menu
+try
+{
+    if (choice>=1||choice<=7)
+        throw exception();
+}
+catch(exception e)
+{
+    cout<<"Error! choose number between 1:7";
+}
+    do
+{
 cout<<"Banking System\n\n";
 cout<<"         Select one Option below\n";
 cout<<"         1.Create new Account\n";
@@ -74,28 +85,25 @@ cout<<"         6.close Account\n";
 cout<<"         7.Exit\n";
 cout<<"Enter your choice";
 cin>>choice;
-try
-{
-    if (choice<1||choice>9)
-        throw exception();
-}
-catch(exception e)
-{
-    cout<<"Error! choose number between 1:9"
-}
-    do
-{
-    switch(choice)
+switch(choice)
     {
 case 1:
     cout<<"Enter your Account name and, Enter your Balance \n";
     getline(cin,name);
     cin>>balance;
+    if(balance<500&&balance>0)
+    {
+        cout<<" put balance more than 500\n";
+        break;
+    }
+    else
+    {
     b.open_acc(name,balance);
     cout<<"Congratulations, The Account is created \n";
-    break;
+     break;
+    }
 case 2:
-    cout<<"Enter your Account number \n"
+    cout<<"Enter your Account number \n";
     cin>>accnum;
     b.show_acc(accnum);
     break;
@@ -103,28 +111,28 @@ case 3:
     cout<<"Enter your Account number and, the Balance you want to add \n";
     cin>>accnum;
     cin>>amount;
-    b.deposit(accnum,amount)
+    b.deposit(accnum,amount);
     break;
 case 4:
     cout<<"Enter your Account number and, the Balance you want to get \n";
     cin>>accnum;
     cin>>amount;
-    b.withdraw(accnum,amount)
+    b.withdraw(accnum,amount);
     break;
 case 5:
-    cout<<"All accounts in the bank \n"
-    b.show_all_accs()
+    cout<<"All accounts in the bank \n";
+    b.show_all_accs();
     break;
 case 6:
-    cout<<"Enter your Account number to remove it \n"
+    cout<<"Enter your Account number to remove it \n";
     cin>>accnum;
-    b.close_acc(accnum)
+    b.close_acc(accnum);
     break;
 case 7:
-    cout<<"thank you for using my Bank,Goodbye \n"
+    cout<<"thank you for using my Bank,Goodbye \n";
     break;
     }
-long Account::next_acc_no=0;
+
 }
 while(choice!=7);
 }
@@ -137,13 +145,13 @@ Account::Account( string n,float bal)
 {
     next_acc_no++;
     acc_no=next_acc_no;
-    name=n;
-    balance=bal;
+    acc_name=n;
+    acc_balance=bal;
 }
 
 long Account:: get_acc_no ()
 {
-    return accountnumber;
+    return acc_no;
 }
 string Account::get_acc_name ()
 {
@@ -153,31 +161,36 @@ float Account::get_acc_balance ()
 	{
 		return acc_balance;
 	}
-float Account::debosit (float amount)
+float Account::deposit (float amount)
 {
     return  acc_balance+=amount;
 }
 float Account::withdraw (float amount)
 {
-    if(acc_balance - amount < Min_BALANCE
-       )
+    if(acc_balance - amount > Min_BALANCE)
     return acc_balance-=amount;
+    else
+    {
+        cout<< "your balance is not enough \n";
+         return 0.0;
+    }
+
 }
-ofstream& operator<< (ofstream &ofs,account &acc)
+ofstream& operator<< (ofstream &ofs,Account &acc)
 	{
 		ofs << "Account Number : " << acc.get_acc_no () << endl;
 		ofs << "Account Name : " << acc.get_acc_name () << endl;
 		ofs << "Account Balance : " << acc.get_acc_balance () <<endl;
 		return ofs;
 	}
-ifstream& operator>> (ifstream &ifs,account &acc)
+ifstream& operator>> (ifstream &ifs,Account &acc)
 	{
 		ifs >> acc.acc_no;
 		getline (ifs, acc.acc_name) ;
 		ifs >> acc.acc_balance;
 		return ifs;
 	}
-ostream& operator<< (ostream &os,account &acc)
+ostream& operator<< (ostream &os,Account &acc)
 	{
 		os << "Account Number : " << acc.get_acc_no () << endl;
 		os << "Account Name : " << acc.get_acc_name () <<endl;
@@ -189,10 +202,10 @@ Bank::Bank ()
 {
     Account acc;
 }
-Account Bank::open_acc (string acc_name, long acc_balance)
+Account Bank::open_acc (string acc_name, float acc_balance)
 {
     Account acc(acc_name, acc_balance);
-    accs_list.insert (pair<long, Account>(acc.get_acc_no(), acc));
+    acc_list.insert (pair<long, Account>(acc.get_acc_no(), acc));
     return acc;
 }
 Account Bank::show_acc (long acc_no)
@@ -212,31 +225,33 @@ Account Bank::withdraw (long acc_no, float amount)
     itr -> second.withdraw(amount);
     return itr -> second;
 }
-Account Bank::close_acc (long acc_no)
+void Bank::close_acc (long acc_no)
 {
     map<long, Account>::iterator itr = acc_list.find(acc_no);
-    acc_list.erase(acc_no)
+    acc_list.erase(acc_no);
 }
 float Bank::total_money ()//to show the sum of all account balance
 {
     float sum=0.0;
-    for(int x=acc_list.begin();x!=acc_list.end();x++)
+    map<long,Account>::iterator itr;
+    for(itr=acc_list.begin(); itr != acc_list.end(); itr++)
     {
         sum += itr -> second.get_acc_balance ();
     }
     return sum;
 }
-void show_all_accs()
+void Bank::show_all_accs()
 {
-    for(x=acc_list.begin();x!=acc_list.end();x++)
+    map<long,Account>::iterator itr;
+    for(itr=acc_list.begin(); itr != acc_list.end(); itr++)
     {
-       cout<< itr -> second.show_acc(x)<<endl;
+       cout<<itr ->second<<endl;
     }
 }
-bool is_acc_exist (long acc_no, string acc_name)
+bool Bank::is_acc_exist (long acc_no)
 {
     map<long, Account>::iterator itr = acc_list.find(acc_no);
-    if(itr -> second.get_acc_name () == acc_name )
+    if(itr -> second.get_acc_no () == acc_no  )
             return true;
     else
     {
